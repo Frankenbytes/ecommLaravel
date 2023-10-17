12861162f9a17a91afdf02426055ce57cad45352
ralVovax <141141823+ralVovax@users.noreply.github.com>
2023-10-17 13:52:32 +0800

    crud functionality of products

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Products::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required',
+        ]);
+
+        $product = Product::create($validateData);
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $product = Product::find($id);
+
+        if($product) {
+            return response()->json($product, 200); //ok
+        } else {
+            return response()->json(['message' => 'Product not found!'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required',
+        ]);
+
+        if($product) {
+            $product->update($validateData);
+            return response()->json($product, 201);
+        } else {
+            return response()->json9(['message' => 'Product not found'], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        if($product) {
+            $product->delete();
+            return respones()->json(null, 204);
+        } else {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+    }
+}
